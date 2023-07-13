3b2a4b62c8c716a8785fecfa139bad1c75a49983
Sarfaraz Ahmed <sarfaraz.ahmed008@hotmail.com>
2023-07-13 22:32:08 +0600

    cors middleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ const config = require('./config');
 const facebookRoutes = require('./routes/facebookRoutes');
 const openAIRoutes = require('./routes/openAIRoutes');
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api/v1/facebook', facebookRoutes);
 app.use('/api/v1/openai', openAIRoutes);
 

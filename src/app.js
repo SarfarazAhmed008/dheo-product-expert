149d1778bb55214e2b2276913be38aba683838ab
Sarfaraz Ahmed <sarfaraz.ahmed008@hotmail.com>
2023-08-09 15:49:34 +0600

    route

--- src/app.js
+++ src/app.js
@@ -15,6 +15,14 @@ app.use(function(req, res, next) {
 app.use('/api/v1/facebook', facebookRoutes);
 app.use('/api/v1/openai', openAIRoutes);
 app.use('/api/v1/setup', configRoutes);
+app.use('/', function(req, res) {
+  try {
+    res.status(200).json({ message: 'Welcome to Dheo Product Expert' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
 const port = process.env.PORT;
 app.listen(port, () => {

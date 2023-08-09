cd0525232c461becc8ee938a07394190df139f09
Sarfaraz Ahmed <sarfaraz.ahmed008@hotmail.com>
2023-08-09 15:36:39 +0600

    config

--- src/config.js
+++ src/config.js
@@ -9,11 +9,11 @@ module.exports = {
     },
     postgres: {
         connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD
+        // host: process.env.DB_HOST,
+        // port: process.env.DB_PORT,
+        // database: process.env.DB_NAME,
+        // user: process.env.DB_USER,
+        // password: process.env.DB_PASSWORD
     },
     openai: {
         apiKey: process.env.API_KEY
